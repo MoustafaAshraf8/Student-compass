75ebea075e97c10e79c7b1399c3173dd5f255a36
Akram Essam <akram.a.e.2002@gmail.com>
2023-08-21 22:10:50 +0300

    deleting some comments

--- studentcompass/lib/mapPage.dart
+++ studentcompass/lib/mapPage.dart
@@ -26,20 +26,9 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Colors.deepPurple,
         title: const Text("MAAAAAAAP NOT FINAAAL"),
       ),
       body: GoogleMap(
@@ -74,11 +63,6 @@ class _MapPageState extends State<MapPage> {
         },
         markers: markers,
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
