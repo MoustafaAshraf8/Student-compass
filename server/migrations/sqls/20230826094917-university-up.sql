1485961171bc9cf39ef982833681abd72963f644
Akram Essam <akram.a.e.2002@gmail.com>
2023-08-30 00:37:55 +0300

    data base location fill universities

--- server/migrations/sqls/20230826094917-university-up.sql
+++ server/migrations/sqls/20230826094917-university-up.sql
@@ -10,4 +10,7 @@ create table university
 insert into university
 (id, name, longtitude,latitude)
 values
-(1,'test',5.0,5.0);
+(1,'Arab academy for science and technology (AAST)',31.309728241554712, 30.063787469627425);
+(2,'Faculty of engineering Alexandria university (FOE)',31.206169053103086, 29.924772948774198);
+(3,'College complex',31.20725276893664, 29.910821976253256);
+(4,'Medical college complex',31.202425880792422, 29.90428811528882);
