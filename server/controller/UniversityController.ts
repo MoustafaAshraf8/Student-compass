cab103d136eaa2a581c1045c65c99491faf81556
MoustafaAshraf8 <mostafa.ashraf8642@gmail.com>
2023-09-03 00:51:55 +0300

    university endpoint modified

--- server/controller/UniversityController.ts
+++ server/controller/UniversityController.ts
@@ -6,7 +6,7 @@ export class UniversityController{
 
    static async getUniversity(req:Request,res:Response,next:NextFunction){
       let result = await UniversityService.getAllUniversity();
-      console.log(result[0].university_name);
+      console.log(result[0]);
       res.statusCode = 200;
       res.json(result);
    }
--- server/schema/sql/University_query.ts
+++ server/schema/sql/University_query.ts
@@ -1,7 +1,14 @@
 export class UniversityQuery{
 
    public static getAllUniversity(){
-      const query = "select university.id as university_id, university.name as university_name, university.longtitude as university_longtitude, university.latitude as university_latitude from university;";
+      const query = `
+      select
+      university.id as university_id,
+      university.name as university_name,
+      university.longtitude as university_longtitude,
+      university.latitude as university_latitude
+      
+      from university;`;
 
       return query;
    }
